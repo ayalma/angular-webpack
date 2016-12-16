db8c2ed62e8b0e91189bf75f75d63c08f61ef614
ayalma <alimohammadi7117@gmail.com>
2016-12-16 12:52:46 +0330

    boilerplate for angular2 and webpack
    l

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by ali on 12/16/16.
+ */
+import { NgModule } from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+@NgModule({
+    imports: [
+        BrowserModule
+    ],
+    declarations: [
+        AppComponent
+    ],
+    bootstrap: [ AppComponent ]
+})
+export class AppModule { }
---
+++ webpack.config.js
@@ -0,0 +1,4 @@
+/**
+ * Created by ali on 12/16/16.
+ */
+module.exports = require('./config/webpack.dev.js');
